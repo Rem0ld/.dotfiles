b768ae8747ffac9f757ceedbc06e3b366f5efaa5
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-11-28 17:57:42 +0100

    fixed error, add script to add symlink and remove them

---
+++ clean_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+unlink ~/.config/starship.toml
+unlink ~/Iterm
+unlink ~/.config/nvim/lua/user
+unlink ~/.zshrc
+unlink ~/.tmux.conf
+unlink /Users/pielov/Library/Application\ Support/Code/User/keybindings.json
+unlink /Users/pielov/Library/Application\ Support/Code/User/settings.json
---
+++ symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s ~/.dotfiles/starship.toml ~/.config
+ln -s ~/.dotfiles/Iterm ~/
+ln -s ~/.dotfiles/.config/nvim/lua/user ~/.config/nvim/lua/
+ln -s ~/.dotfiles/.zshrc ~/
+ln -s ~/.dotfiles/.tmux.conf ~/
+ln -s ~/.dotfiles/VsCode/settings.json /Users/pielov/Library/Application\ Support/Code/User
+ln -s ~/.dotfiles/VsCode/keybindings.json /Users/pielov/Library/Application\ Support/Code/User
