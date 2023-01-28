4c0ba265b76eff61e91d7829c8c77026d2d7b20f
Pierre Lovergne <pierre.lovergne@gmail.com>
2023-01-28 15:53:58 +0100

    add new config nvim to dotfiles

---
+++ nvim/lua/v/config/init.lua
@@ -0,0 +1,5 @@
+require("v.config.options")
+require("v.config.autocmds")
+require("v.config.keymaps")
+require("v.config.lazy")
+require("v.config.utils")
---
+++ nvim/lua/v/plugins/lualine.lua
@@ -0,0 +1,64 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = function(plugin)
+    -- local icons = require("lazyvim.config").icons
+
+    local function fg(name)
+      return function()
+        ---@type {foreground?:number}?
+        local hl = vim.api.nvim_get_hl_by_name(name, true)
+        return hl and hl.foreground and { fg = string.format("#%06x", hl.foreground) }
+      end
+    end
+
+    return {
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {
+          {
+            "diagnostics",
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          {
+            "filename",
+            path = 1,
+            shorting_target = 40,
+            symbols = { modified = "  ", readonly = "", unnamed = "" },
+          },
+          { "searchcount" },
+        },
+        lualine_x = { "encoding", "filetype" },
+        lualine_y = {
+                    -- stylua: ignore
+                    {
+                        function() return require("noice").api.status.command.get() end,
+                        cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+                        color = fg("Statement")
+                    },
+                    -- stylua: ignore
+                    {
+                        function() return require("noice").api.status.mode.get() end,
+                        cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+                        color = fg("Constant"),
+                    },
+          { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = fg("Special") },
+          {
+            "diff",
+          },
+        },
+        lualine_z = {
+          { "progress", separator = "", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+      },
+      extensions = { "neo-tree" },
+    }
+  end,
+}
