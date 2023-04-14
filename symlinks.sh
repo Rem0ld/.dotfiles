15700666737c6d147c222252af00cc0c4702a2e2
Pierrot <pierre.lovergne@gmail.com>
2023-04-14 08:46:53 +0200

    add symlink script linux

--- symlinks.sh
+++ symlinks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ln -s ~/.dotfiles/starship.toml ~/.config
+ln -s ~/.dotfiles/.zshrc ~/
 ln -s ~/.dotfiles/Iterm ~/
 ln -s ~/.dotfiles/config/nvim_astronvim/lua/user ~/.config/nvim/lua/
-ln -s ~/.dotfiles/.zshrc ~/
 ln -s ~/.dotfiles/.tmux.conf ~/
 ln -s ~/.dotfiles/VsCode/settings.json /Users/pielov/Library/Application\ Support/Code/User
 ln -s ~/.dotfiles/VsCode/keybindings.json /Users/pielov/Library/Application\ Support/Code/User
---
+++ symlinks_linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -sf ~/.dotfiles/starship.toml ~/.config
+ln -sf ~/.dotfiles/.zshrc ~/
+ln -sf ~/.dotfiles/.xinitrc ~/
+ln -sf ~/.dotfiles/nvim ~/.config
+ln -sf ~/.dotfiles/config/i3 ~/.config
+ln -sf ~/.dotfiles/config/polybar ~/.config
+ln -sf ~/.dotfiles/config/kitty ~/.config
+ln -sf ~/.dotfiles/robbyrussell.zsh-theme ~/.oh-my-zsh/themes
