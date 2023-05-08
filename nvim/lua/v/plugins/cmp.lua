c10015f31e32e6809a337d06ab6fd587c5f8b125
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2023-05-08 07:44:24 +0200

    ui nvim cmp

--- nvim/lua/v/plugins/cmp.lua
+++ nvim/lua/v/plugins/cmp.lua
@@ -1,146 +1,148 @@
 local kind_icons = {
-    Text = "",
-    Method = "",
-    Function = "",
-    Constructor = "ﰕ",
-    Field = "ﰠ",
-    Variable = "",
-    Class = "ﴯ",
-    Interface = "",
-    Module = "",
-    Property = "",
-    Unit = "塞",
-    Value = "",
-    Enum = "",
-    Keyword = "",
-    Snippet = "",
-    Color = "",
-    File = "",
-    Reference = "",
-    Folder = "",
-    EnumMember = "",
-    Constant = "",
-    Struct = "פּ",
-    Event = "",
-    Operator = "",
-    TypeParameter = "",
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "ﰕ",
+  Field = "ﰠ",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "塞",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "פּ",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
 }
 
 local function border(hl_name)
-    return {
-        { "╭", hl_name },
-        { "─", hl_name },
-        { "╮", hl_name },
-        { "│", hl_name },
-        { "╯", hl_name },
-        { "─", hl_name },
-        { "╰", hl_name },
-        { "│", hl_name },
-    }
+  return {
+    { "╭", hl_name },
+    { "─", hl_name },
+    { "╮", hl_name },
+    { "│", hl_name },
+    { "╯", hl_name },
+    { "─", hl_name },
+    { "╰", hl_name },
+    { "│", hl_name },
+  }
 end
 
 return {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
 
-        "hrsh7th/cmp-vsnip",
-        "hrsh7th/vim-vsnip",
-        "onsails/lspkind.nvim",
-        -- "rafamadriz/friendly-snippets",
-        -- "kitagry/vs-snippets",
-        -- "burkeholland/simple-react-snippets",
-    },
-    config = function()
-        local cmp = require("cmp")
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
+    "onsails/lspkind.nvim",
+    -- "rafamadriz/friendly-snippets",
+    -- "kitagry/vs-snippets",
+    -- "burkeholland/simple-react-snippets",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-        cmp.setup({
-            enabled = function()
-                local in_prompt = vim.api.nvim_buf_get_option(0, "buftype") == "prompt"
-                if in_prompt then
-                    return false
-                end
-                local context = require("cmp.config.context")
-                return not (
-                    context.in_treesitter_capture("comment") == true
-                    or context.in_syntax_group("Comment")
-                    )
-            end,
-            window = {
-                completion = {
-                    winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
-                    col_offset = -3,
-                    side_padding = 0,
-                },
-                documentation = {
-                    border = border("CmpDocBorder"),
-                    winhighlight = "Normal:CmpDoc",
-                },
-            },
-            formatting = {
-                fields = { "kind", "abbr", "menu" },
-                format = function(entry, vim_item)
-                    local kind = require("lspkind").cmp_format({
-                        mode = "symbol_text",
-                        maxwidth = 50,
-                    })(entry, vim_item)
-                    local strings = vim.split(kind.kind, "%s", { trimempty = true })
-                    kind.kind = " " .. (strings[1] or "") .. " "
-                    kind.menu = "    (" .. (strings[2] or "") .. ")"
+    cmp.setup({
+      enabled = function()
+        local in_prompt = vim.api.nvim_buf_get_option(0, "buftype") == "prompt"
+        if in_prompt then
+          return false
+        end
+        local context = require("cmp.config.context")
+        return not (
+          context.in_treesitter_capture("comment") == true
+          or context.in_syntax_group("Comment")
+        )
+      end,
+      window = {
+        completion = {
+          border = border("CmpDocBorder"),
+          winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
+          col_offset = -3,
+          side_padding = 1,
+        },
 
-                    return kind
-                end,
-            },
-            snippet = {
-                expand = function(args)
-                    vim.fn["vsnip#anonymous"](args.body)
-                end,
-            },
-            mapping = {
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                ["<C-j>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<C-k>"] = cmp.mapping(function()
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    end
-                end, { "i", "s" }),
-                ["<Down>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<Up>"] = cmp.mapping(function()
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    end
-                end, { "i", "s" }),
-            },
-            sources = {
-                { name = "nvim_lsp" },
-                { name = "nvim_lua" },
-                { name = "path" },
-                { name = "buffer", },
-                { name = "vsnip" },
-            },
-        })
-    end,
+        documentation = {
+          border = border("CmpDocBorder"),
+          winhighlight = "Normal:CmpDoc",
+        },
+      },
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          local kind = require("lspkind").cmp_format({
+            mode = "symbol_text",
+            maxwidth = 80,
+          })(entry, vim_item)
+          local strings = vim.split(kind.kind, "%s", { trimempty = true })
+          kind.kind = " " .. (strings[1] or "") .. " "
+          kind.menu = " (" .. (strings[2] or "") .. ")"
+
+          return vim_item
+        end,
+      },
+      snippet = {
+        expand = function(args)
+          vim.fn["vsnip#anonymous"](args.body)
+        end,
+      },
+      mapping = {
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-j>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<C-k>"] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_prev_item()
+          end
+        end, { "i", "s" }),
+        ["<Down>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<Up>"] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_prev_item()
+          end
+        end, { "i", "s" }),
+      },
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "path" },
+        { name = "buffer" },
+        { name = "vsnip" },
+      },
+    })
+  end,
 }
