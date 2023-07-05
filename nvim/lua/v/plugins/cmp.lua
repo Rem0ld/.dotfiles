2b0b3d8f2791c32a5679924cf487cdb04dc78fd5
Pierre Lovergne <pierre.lovergne@gmail.com>
2023-07-05 09:10:54 +0200

    remove unused stuff and format files

--- nvim/lua/v/plugins/cmp.lua
+++ nvim/lua/v/plugins/cmp.lua
@@ -1,31 +1,3 @@
-local kind_icons = {
-  Text = "",
-  Method = "",
-  Function = "",
-  Constructor = "ﰕ",
-  Field = "ﰠ",
-  Variable = "",
-  Class = "ﴯ",
-  Interface = "",
-  Module = "",
-  Property = "",
-  Unit = "塞",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "פּ",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
-}
-
 local function border(hl_name)
   return {
     { "╭", hl_name },
