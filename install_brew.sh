f76f1d015988f1b4d2f6b77730543a274c995c47
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2023-07-12 09:47:29 +0200

    add symbolic link

--- install_brew.sh
+++ install_brew.sh
@@ -9,8 +9,3 @@ if [[ $result != 0 ]]; then
 	echo "Something wrong happened when installing brew"
 	exit 1;
 fi
-
-brew install --cask iterm2 brave-browser visual-studio-code obsidian insomnia discord slack bitwarden alfred rectangle font-fira-code-nerd-font &&
-brew install git node nvm mysql postgres ripgrep starship docker;
-
-exit 0;
--- symlinks.sh
+++ symlinks.sh
@@ -2,9 +2,10 @@
 
 ln -s ~/.dotfiles/starship.toml ~/.config
 ln -s ~/.dotfiles/config/gitui ~/.config
+ln -s ~/.dotfiles/config/yabai ~/.config
+ln -s ~/.dotfiles/config/skhd ~/.config
 ln -s ~/.dotfiles/.zshrc ~/
 ln -s ~/.dotfiles/Iterm ~/
-ln -s ~/.dotfiles/config/nvim_astronvim/lua/user ~/.config/nvim/lua/
 ln -s ~/.dotfiles/.tmux.conf ~/
 ln -s ~/.dotfiles/VsCode/settings.json /Users/pielov/Library/Application\ Support/Code/User
 ln -s ~/.dotfiles/VsCode/keybindings.json /Users/pielov/Library/Application\ Support/Code/User
