ec9fa52429d2c629b952958ba94c08770eecfc32
Pierre Lovergne <pierre.lovergne@gmail.com>
2023-08-02 16:27:07 +0200

    redid script to link or clear files from dotfiles

--- clean_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-unlink ~/.config/starship.toml
-unlink ~/Iterm
-unlink ~/.config/nvim/lua/user
-unlink ~/.zshrc
-unlink ~/.tmux.conf
-unlink /Users/pielov/Library/Application\ Support/Code/User/keybindings.json
-unlink /Users/pielov/Library/Application\ Support/Code/User/settings.json
