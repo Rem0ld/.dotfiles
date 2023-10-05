b203a670da23338956c0a49fd85de3cbe28bb4e5
Pierre Lovergne <pierre.lovergne@gmail.com>
2023-10-05 22:01:54 +0200

    add session

---
+++ config/nvim/lua/v/plugins/auto-session.lua
@@ -0,0 +1,12 @@
+return {
+  "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup({
+      log_level = "error",
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      auto_session_enabled = true,
+      auto_save_enabled = true,
+      auto_restore_enabled = true,
+    })
+  end,
+}
