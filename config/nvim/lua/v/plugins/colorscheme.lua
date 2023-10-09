f2542bb07154c31ab4373277c6e3bb150db28123
Pierre Lovergne <pierre.lovergne@gmail.com>
2023-10-09 12:07:19 +0200

    update colorscheme

--- config/nvim/lua/v/plugins/colorscheme.lua
+++ config/nvim/lua/v/plugins/colorscheme.lua
@@ -13,18 +13,18 @@ return {
     lazy = false,
     -- opts = { style = "storm" },
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme nightfly]])
-    end,
+    -- config = function()
+    --   -- load the colorscheme here
+    --   vim.cmd([[colorscheme nightfly]])
+    -- end,
   },
   {
     "EdenEast/nightfox.nvim",
     lazy = false,
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme nightfox]])
-    end,
+    -- config = function()
+    --   -- load the colorscheme here
+    --   vim.cmd([[colorscheme nightfox]])
+    -- end,
   },
 }
--- config/nvim/lua/v/plugins/notify.lua
+++ config/nvim/lua/v/plugins/notify.lua
@@ -1,6 +1,7 @@
 return {
   "rcarriga/nvim-notify",
   opts = {
+    background_colour = "#1e2030",
     top_down = false,
     timeout = 2000,
     max_height = 10,
