69135e2e9da99781d3878da96e02969448cb41b0
Pierre Lovergne <pierre.lovergne@gmail.com>
2023-11-03 14:05:41 +0100

    change telescope default

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -2,24 +2,12 @@ return {
   "folke/which-key.nvim",
   opts = {
     defaults = {
+      triggers_blacklist = {
+        i = { "<" },
+      },
       ["<leader>"] = {
         ["e"] = nil,
         ["E"] = nil,
-        ["f"] = {
-          name = "Telescope",
-          ["f"] = { "<CMD>Telescope find_files<CR>", "Find File" },
-          ["F"] = {
-            "<CMD>Telescope find_files hidden=true no_ignore=true<CR>",
-            "Find all File",
-          },
-          ["w"] = { "<CMD>Telescope live_grep<CR>", "Find File by grep" },
-          ["W"] = {
-            "<CMD>Telescope live_grep hidden=true no_ignore=true<CR>",
-            "Find File by grep",
-          },
-          ["b"] = { "<CMD>Telescope buffers<CR>", "Find buffers" },
-          ["d"] = { "<CMD>Telescope diagnostics<CR>", "Diagnostics" },
-        },
         ["g"] = {
           name = "Git",
           ["g"] = {
